9c0dcc841f353e6c5f43f05618b238964ead9228
Afan Alfi Andi <alfiandiafan@gmail.com>
2024-11-25 01:07:58 +0700

    [add] create specialist

--- app/Http/Controllers/SpecialistController.php
+++ app/Http/Controllers/SpecialistController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Specialist;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class SpecialistController extends Controller
 {
@@ -15,5 +16,34 @@ class SpecialistController extends Controller
             'status' => 'success',
             'data' => $specialist
         ], 200);
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'specialistName'   => 'required|string|max:50',
+            'img'        => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
+        ]);
+
+        try {
+            $imgName = 'default.png';
+            if ($request->hasFile('img')) {
+                $imgName = md5(uniqid() . '-' . time()) . '.' . $request->file('img')->getClientOriginalExtension();
+
+                $request->file('img')->storeAs('images/specialist', $imgName, 'public');
+            }
+
+            Specialist::create([
+                'specialistName'   => $validated['specialistName'],
+                'img'        => $imgName,
+            ]);
+
+            return response()->json(['message' => 'success'], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'failed',
+                'error'   => $e->getMessage(),
+            ], 500);
+        }
     }
 }
