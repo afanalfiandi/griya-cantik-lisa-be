7c7a411230d45befad3f89c8b3af2794908b8517
afanalfiandi <alfiandiafan@gmail.com>
2024-12-15 21:14:47 +0700

    [update] sidear icon

--- app/Filament/Resources/BannerResource.php
+++ app/Filament/Resources/BannerResource.php
@@ -18,7 +18,7 @@ class BannerResource extends Resource
 {
     protected static ?string $model = Banner::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/SpecialistResource.php
+++ app/Filament/Resources/SpecialistResource.php
@@ -18,7 +18,7 @@ class SpecialistResource extends Resource
 {
     protected static ?string $model = Specialist::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     public static function form(Form $form): Form
     {
